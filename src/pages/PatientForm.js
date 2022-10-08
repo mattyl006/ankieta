6de5a79cbedc5734cf6d19578e67fb2cd7f0ed63
Wirusik <wirus006@gmail.com>
2022-10-09 00:57:51 +0200

    after ankiet

--- src/pages/PatientForm.js
+++ src/pages/PatientForm.js
@@ -4,23 +4,36 @@ import {H1, H2, Body, Medium} from "../utils/fonts";
 import {FlexColumn, FlexRow} from "../utils/containers";
 
 const PatientForm = (props) => {
+    React.useEffect(() => {
+        setTimeout(() => {
+            window.location.reload()
+        }, 3000);
+    });
+
     return (
         <FlexColumn width='100%' gap='36px'>
             <H1 as='h1' color={theme.colors.brand}>
                 Ankieta pacjenta
             </H1>
-            {Object.entries(props.data).map((elem, index) => {
-                return (
-                    <FlexRow key={index}>
-                        <Medium>
-                            {elem[0]}:
-                        </Medium>
-                        <Body>
-                            {elem[1]}
-                        </Body>
-                    </FlexRow>
-                )
-            })}
+            <Medium>
+                Ankieta przesłana, dziękujemy za jej wypełnienie
+            </Medium>
+            {/* <FlexColumn width='' alignmentX='flex-start'>
+                {Object.entries(props.data).map((elem, index) => {
+                    if (elem[0] !== 'result') {
+                        return (
+                            <FlexRow key={index}>
+                                <Medium>
+                                    {elem[0]}:
+                                </Medium>
+                                <Body>
+                                    {elem[1]}
+                                </Body>
+                            </FlexRow>
+                        )
+                    }
+                })}
+            </FlexColumn> */}
         </FlexColumn>
     );
 };
